9afb2703b37e87cc8f059a4d1586a72a36a8c1be
Nhat Hoang <hoangnh092185@gmail.com>
2017-03-31 19:25:25 -0700

    initial commit.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import './App.css';
+
+import '../semantic/dist/semantic.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
